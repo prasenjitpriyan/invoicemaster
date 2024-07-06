ff5bff2606f251f8c83b39945c687d09d01b252b
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-06 20:57:20 +0530

    Theme switcher modified

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import MoonIcon from "@/assets/icon-moon.svg";
+import SunIcon from "@/assets/icon-sun.svg";
+import { useTheme } from "@/lib/features/Theme/useTheme";
+import Image from "next/image";
 
 const ThemeSwitcher: React.FC = () => {
-  return <div>ThemeSwitcher</div>;
+  const [theme, setTheme] = useTheme();
+
+  const toggleTheme = () => {
+    setTheme();
+  };
+  return (
+    <div onClick={toggleTheme} className="cursor-pointer">
+      {theme === "dark" ? (
+        <Image
+          width={100}
+          height={100}
+          src={MoonIcon}
+          alt="switcher moon icon"
+        />
+      ) : (
+        <Image width={100} height={100} src={SunIcon} alt="switcher sun icon" />
+      )}
+    </div>
+  );
 };
 
 export default ThemeSwitcher;
